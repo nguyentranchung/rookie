e3a18db010c549f71c471ecd0fbe55e7caacdc6a
Nguyễn Trần Chung <nguyentranchung52th@gmail.com>
2020-09-06 19:24:58 +0900

    route name

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,6 @@
 <?php
 
 Route::group(config('rookie.route', []), function () {
-    Route::get('{rookieName}', '\NguyenTranChung\Rookie\Http\Controllers\RookieController@index');
+    Route::get('{rookieName}', '\NguyenTranChung\Rookie\Http\Controllers\RookieController@index')
+        ->name('rookie.index');;
 });
