4a29c0896058c6a19f9acd6ed52fc7a39c633a90
Nguyễn Trần Chung <nguyentranchung52th@gmail.com>
2020-09-06 18:27:11 +0900

    js

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,10 +32,12 @@
 
 @livewireScripts
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js"
-        integrity="sha512-M5KW3ztuIICmVIhjSqXe01oV2bpe248gOxqmlcYrEzAvws7Pw3z6BK0iGbrwvdrUQUhi3eXgtxp5I8PDo9YfjQ==" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
-        integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.6.0/alpine.js"
         integrity="sha512-f1s2tx564OL4dSUFbTb9EE9skCfMIYl11V3CbDoM/cQN5UcMUsAacoekIExvl89ZW5oalPL5UPKs4x71XNSq0g==" crossorigin="anonymous"></script>
 
@@ -47,6 +49,11 @@
             console.log('test', JSON.stringify(param));
         });
     });
+    $(document).ready(function() {
+        $('.dropdown-menu.keep-open').on('click', function(e) {
+            e.stopPropagation()
+        })
+    })
 </script>
 </body>
 </html>
